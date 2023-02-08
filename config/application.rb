139109f4ae876c1dcffe54e1a845f146abb3b572
MariaKypraiou <mariakypraiou12@gmail.com>
2023-02-08 18:00:36 +0200

    Configure and create helpers - Change include_all_helpers config to false - Split the _collapsible_elements.html.erb file's content into   partials and extract logic from the file into partials

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Collabfield
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.action_controller.include_all_helpers = false
+        
   end
 end

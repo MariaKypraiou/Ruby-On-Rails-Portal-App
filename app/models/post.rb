7ed9f6c3376a55a099b825726c34ed220f7c9138
MariaKypraiou <mariakypraiou12@gmail.com>
2023-02-13 21:06:17 +0200

    -Generate Post & Category models, -Create associations between User,Port and Category models, -Create categories and posts database tables.

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+    belongs_to :user
+  belongs_to :category
+end
+class User < ApplicationRecord
+  ...
+  has_many :posts, dependent: :destroy       
+end
+
+class Category < ApplicationRecord
+  has_many :posts
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_223153) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_13_185903) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "branch"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "category_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
